dab8d6b935ed9489a2076e153e6690099da8ff5a
Christopher Aaron <christops.codes@gmail.com>
2024-08-18 16:42:12 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const tmi = require('tmi.js');
+
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const commands = {
+	website: {
+		response: 'https://twitch.tv/TOPSilog_'
+	}/* ,
+	upvote: {
+		response: (user) => `User ${user} was just upvoted`
+	} */
+}
+
+const client = new tmi.Client({
+	channels: [ 'TOPSilog_' ],
+    identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	}
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME;
+
+    if (!isNotBot) return;
+
+    const [raw, command, argument] = message.match(regexpCommand);
+
+	const { response } = commands[command] || {};
+
+	if ( typeof response === 'function') {
+		client.say(channel, response(tags.username))
+	} else if ( typeof response === 'string' ) {
+		client.say(channel, response);
+	}
+
+
+    // "TOPSilog_: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
